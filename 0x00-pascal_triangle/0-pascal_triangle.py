94b7068a9b479afe4cbf7316bf8060288e80a4fb
zurumlab <akanduj59@yahoo.com>
2023-07-02 22:47:41 +0000

    ....

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -17,3 +17,12 @@ def pascal_triangle(n):
         triangle.append(row)
 
     return triangle
+
+
+def print_triangle(triangle):
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
