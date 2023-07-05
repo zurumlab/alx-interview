9751362cbe8f38db30cbdb64c083ab627a12637c
zurumlab <akanduj59@yahoo.com>
2023-07-05 22:09:24 +0000

    .....

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,13 @@
-#!/usr/bin/env python3
-
-def pascal_triangle(n):
-    if n <= 0:
-        return []
-
-    triangle = [[1]]
-
-    for i in range(1, n):
-        row = [1]
-        prev_row = triangle[i-1]
-
-        for j in range(1, i):
-            row.append(prev_row[j-1] + prev_row[j])
-
-        row.append(1)
-        triangle.append(row)
-
-    return triangle
-
+#!/usr/bin/python3
+"""
+0-main
+"""
+pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 
 def print_triangle(triangle):
+    """
+    Print the triangle
+    """
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
 
